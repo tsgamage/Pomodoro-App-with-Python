898394d19a6b67b305dd8b6afda49d64e7243503
tsgamage <thevidu2007627@gmail.com>
2025-06-02 11:26:44 +0530

    Make the timer functions

--- main.py
+++ main.py
@@ -17,6 +17,29 @@ WORKING_TIME = 25 * 60
 SMALL_BREAK = 5 * 60
 LONG_BREAK = 20 * 60
 
+
+def timer(count):
+    timer_min = math.floor(count / 60)
+    timer_sec = count % 60
+
+    if timer_min < 10:
+        timer_min = f"0{timer_min}"
+
+    if timer_sec < 10:
+        timer_sec = f"0{timer_sec}"
+
+    if timer_sec == 0:
+        timer_sec = "00"
+
+    canvas.itemconfig(timer_text, text=f"{timer_min}:{timer_sec}")
+
+    if count > 0:
+        window.after(1000, timer, count - 1)
+
+def call_timer():
+    timer(300)
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro App")
@@ -38,7 +61,7 @@ canvas = Canvas()
 canvas.config(bg=GREEN, width=250, height=224, highlightthickness=0)
 tomato_image = PhotoImage(file="./assets/tomato.png")
 canvas.create_image(125, 112, image=tomato_image)
-canvas.create_text(125, 130, text="00:00", font=(FONT_NAME, 36), fill="white")
+timer_text = canvas.create_text(125, 130, text="00:00", font=(FONT_NAME, 36), fill="white")
 canvas.grid(row=1, column=0, columnspan=2)
 
 checkmark_label = Label(text="✔✔✔")
@@ -47,7 +70,7 @@ checkmark_label.grid(row=2, column=0, columnspan=2, pady=(10, 0))
 
 start_btn = Button(text="Start")
 start_btn.config(width=10, height=1, font=(FONT_NAME, 18), bg=PINK, fg='white', activebackground=DARK_PINK,
-                 activeforeground='white')
+                 activeforeground='white', command=call_timer)
 start_btn.grid(row=3, column=0, padx=5, pady=10)
 
 reset_btn = Button(text="Reset")
